45efc7a7b4b8c5bcb21a2ed288888543924a795d
David Flemström <david.flemstrom@gmail.com>
2018-10-26 12:23:56 +0200

    Re-use parser in the fuzzer

--- norm-parser-fuzz-target/src/main.rs
+++ norm-parser-fuzz-target/src/main.rs
@@ -2,11 +2,13 @@
 extern crate afl;
 extern crate norm;
 
+use std::panic;
+
 fn main() {
-    fuzz!(|data: &[u8]| {
-        if let Ok(s) = std::str::from_utf8(data) {
-            use norm::parser::Parse;
-            let _ = norm::ast::Module::parse(s);
-        }
+    use norm::parser::Parse;
+    let parser = panic::AssertUnwindSafe(norm::ast::Module::new_parser());
+
+    fuzz!(|data: &[u8]| if let Ok(s) = std::str::from_utf8(data) {
+        let _ = parser.parse(s);
     });
 }
