4771620d9ddcaf05d5d54e78976f2c69ba92d476
David Flemström <david.flemstrom@gmail.com>
2018-12-05 22:12:58 +0100

    Remove extern "C" specifier

--- src/builtin.rs
+++ src/builtin.rs
@@ -11,14 +11,14 @@ pub const SYMBOLS: &[(&str, *const u8)] = &[
     (DEALLOC_SYMBOL, norm_dealloc as *const u8),
 ];
 
-unsafe extern "C" fn norm_alloc(size: usize, align: num::NonZeroUsize) -> *mut u8 {
+unsafe extern fn norm_alloc(size: usize, align: num::NonZeroUsize) -> *mut u8 {
     let layout = alloc::Layout::from_size_align_unchecked(size, align.get());
     let ptr = alloc::alloc(layout);
     debug!("alloc size={:?} align={:?} ptr={:?}", size, align, ptr);
     ptr
 }
 
-unsafe extern "C" fn norm_dealloc(ptr: *mut u8, size: usize, align: num::NonZeroUsize) {
+unsafe extern fn norm_dealloc(ptr: *mut u8, size: usize, align: num::NonZeroUsize) {
     let layout = alloc::Layout::from_size_align_unchecked(size, align.get());
     debug!("dealloc ptr={:?} size={:?} align={:?}", ptr, size, align);
     alloc::dealloc(ptr, layout);
