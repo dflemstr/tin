3febe69938a3cb3a6e47f2caa7d79ca249c501a9
David Flemström <david.flemstrom@gmail.com>
2018-12-12 23:12:26 +0100

    Disable graph rendering by default

--- src/test_util.rs
+++ src/test_util.rs
@@ -3,17 +3,19 @@ use failure;
 use ir;
 
 pub fn render_graph(name: &str, ir: &ir::Ir) -> Result<(), failure::Error> {
-    let graph = ir::graph::Graph::new(ir);
+    if cfg!(feature = "test-render-graphs") {
+        let graph = ir::graph::Graph::new(ir);
 
-    let dot_path = format!("/tmp/{}.dot", name);
-    let img_path = format!("/tmp/{}.svg", name);
+        let dot_path = format!("/tmp/{}.dot", name);
+        let img_path = format!("/tmp/{}.svg", name);
 
-    let mut file = ::std::fs::File::create(&dot_path)?;
-    dot::render(&graph, &mut file)?;
-    ::std::process::Command::new("dot")
-        .args(&["-Tsvg", "-o", &img_path, &dot_path])
-        .spawn()?
-        .wait()?;
+        let mut file = ::std::fs::File::create(&dot_path)?;
+        dot::render(&graph, &mut file)?;
+        ::std::process::Command::new("dot")
+            .args(&["-Tsvg", "-o", &img_path, &dot_path])
+            .spawn()?
+            .wait()?;
+    }
 
     Ok(())
 }
