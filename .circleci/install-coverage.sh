9a8a84624f28b2615e587ef7aa9336aa16c06063
David Flemström <david.flemstrom@gmail.com>
2018-12-12 23:50:16 +0100

    Attempt to fix coverage

--- .circleci/install-coverage.sh
+++ .circleci/install-coverage.sh
@@ -1,10 +1,7 @@
 #!/bin/bash
 
 sudo apt-get update
-sudo apt-get install -y curl file gcc g++ git make openssh-client \
-    autoconf automake cmake libtool libcurl4-openssl-dev libssl-dev \
-    libelf-dev libdw-dev binutils-dev zlib1g-dev libiberty-dev wget \
-    xz-utils pkg-config python
+sudo apt-get install -y libcurl4-openssl-dev libelf-dev libdw-dev cmake binutils-dev libiberty-dev
 
 KCOV_VERSION=33
 wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV_VERSION.tar.gz && \
@@ -12,5 +9,5 @@ wget https://github.com/SimonKagstrom/kcov/archive/v$KCOV_VERSION.tar.gz && \
     rm v$KCOV_VERSION.tar.gz && \
     cd kcov-$KCOV_VERSION && \
     mkdir build && cd build && \
-    cmake .. && make && sudo make install && \
+    cmake .. && make -j && sudo make install -j && \
     cd ../.. && rm -rf kcov-$KCOV_VERSION
